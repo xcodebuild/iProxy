5f8520b6353d9266a91f31605042432910d9daad
shejinxin <shejinxin@corp.netease.com>
2021-07-10 23:10:26 +0800

    change flow name

--- scripts/date-version.js
+++ scripts/date-version.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const packageJSON = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'));
 
-packageJSON.version = packageJSON.version + '-beta-' + Date.now();
+packageJSON.version = packageJSON.version + '-test-' + Date.now();
 
 fs.writeFileSync(path.join(__dirname, '../package.json'), JSON.stringify(packageJSON), 'utf-8');
 console.log('generate package.json');
