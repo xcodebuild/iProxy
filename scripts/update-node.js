681f2ad1532db38413bee1fd0543358b2a36bb77
xcodebuild <me@xcodebuild.com>
2021-07-12 20:25:08 +0800

    build for Apple M1

--- scripts/update-node.js
+++ scripts/update-node.js
@@ -6,8 +6,12 @@ const shell = require('shelljs');
 const systemType = os.type();
 const SYSTEM_IS_MACOS = systemType === 'Darwin';
 
+const VERSION = 'v16.4.2';
+
+const IS_ARM = process.env.IS_ARM === '1';
+
 if (SYSTEM_IS_MACOS) {
-    shell.exec(`cd vendor/files/node && curl https://cdn.npm.taobao.org/dist/node/v15.12.0/node-v15.12.0-darwin-x64.tar.gz| tar -xz && mv node-*-darwin-x64/bin/node node-mac && rm -rf node-*-darwin-x64`);
+    shell.exec(`cd vendor/files/node && curl https://cdn.npm.taobao.org/dist/node/${VERSION}/node-${VERSION}-darwin-${IS_ARM ? 'arm64': 'x64'}.tar.gz| tar -xz && mv node-*-darwin-*/bin/node node-mac && rm -rf node-*-darwin-*`);
 } else {
-    shell.exec(`cd vendor/files/node && curl https://cdn.npm.taobao.org/dist/node/v14.15.3/win-x86/node.exe > node-win.exe`)
+    shell.exec(`cd vendor/files/node && curl https://cdn.npm.taobao.org/dist/node/${VERSION}/win-x86/node.exe > node-win.exe`)
 }
